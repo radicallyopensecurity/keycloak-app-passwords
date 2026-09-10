bd8ca874699b5d2c7be56376491bb611d11173f4
syrosh <syrosh@radicallyopensecurity.com>
2026-09-10 17:57:16 +0200

    test(e2e): fix tests and bump dependencies

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   port: 8888,
+  defaultBrowser: 'chrome',
   e2e: {
     specPattern: 'tests/**/*.cy.ts',
     supportFile: false,
--- e2e/tests/e2e.cy.ts
+++ e2e/tests/e2e.cy.ts
@@ -11,15 +11,8 @@ before(() => {
 
 describe('keycloak-app-passwords', () => {
   it('e2e', () => {
-    cy.intercept(
-      'POST',
-      'http://localhost:8080/realms/myrealm/app-password',
-    ).as('regenerate')
-
-    cy.intercept(
-      'DELETE',
-      'http://localhost:8080/realms/myrealm/app-password',
-    ).as('delete')
+    cy.intercept('POST', '**/realms/myrealm/app-password').as('regenerate')
+    cy.intercept('DELETE', '**/realms/myrealm/app-password').as('delete')
 
     cy.log('It can access the app passwords console')
     cy.viewport(1920, 1080)
@@ -33,71 +26,103 @@ describe('keycloak-app-passwords', () => {
 
     cy.get('[data-testid=app-passwords]').click()
 
-    cy.log('It can generates password for the first time')
+    cy.log('It can generate a password for the first time')
     cy.get('[data-testrole=regenerate]').first().click()
     cy.wait('@regenerate')
 
     cy.log('It returns a hidden password')
-    cy.get('[data-testid=app-passwords-value]').contains(HIDDEN_PASSWORD)
+    cy.get('[data-testid=app-passwords-value]').should(
+      'contain.text',
+      HIDDEN_PASSWORD,
+    )
 
     cy.log('It can reveal the password')
-    let password1 = ''
-    cy.get('[data-testid=app-passwords-reveal').first().click()
+    cy.get('[data-testid=app-passwords-reveal]').first().click()
+
     cy.get('[data-testid=app-passwords-value]')
       .invoke('text')
       .then((password) => {
-        expect(password.length).to.eq(32)
+        expect(password).to.have.length(32)
         expect(password).to.not.eq(HIDDEN_PASSWORD)
 
-        password1 = password
+        cy.wrap(password).as('password1')
+
+        cy.window().then((win) => {
+          cy.stub(win.navigator.clipboard, 'writeText').as('clipboardWrite')
+        })
 
         cy.log('It can copy the password')
-        cy.get('[data-testid=app-passwords-copy').first().click()
+        cy.get('[data-testid=app-passwords-copy]').first().click()
 
-        return cy.window().then((win) => {
-          return win.navigator.clipboard.readText().then((clipboard) => {
-            expect(clipboard).to.eq(password)
-          })
-        })
+        cy.get('@clipboardWrite').should('have.been.calledOnceWith', password)
       })
 
     cy.log('It can hide the password again')
-    cy.get('[data-testid=app-passwords-reveal').first().click()
-    cy.get('[data-testid=app-passwords-value]').contains(HIDDEN_PASSWORD)
+    cy.get('[data-testid=app-passwords-reveal]').first().click()
+
+    cy.get('[data-testid=app-passwords-value]').should(
+      'contain.text',
+      HIDDEN_PASSWORD,
+    )
 
     cy.log('It can regenerate')
     cy.get('[data-testrole=regenerate]').first().click()
-    cy.get('[data-testid=confirm]').click()
-    cy.wait('@regenerate')
-    cy.get('[data-testid=app-passwords-reveal').first().click()
-    cy.get('[data-testid=app-passwords-value]')
-      .invoke('text')
-      .then((password) => {
-        expect(password.length).to.eq(32)
-        expect(password).to.not.eq(password1)
-        expect(password).to.not.eq(HIDDEN_PASSWORD)
-      })
 
-    cy.log('It can generate 2nd')
+    cy.get('[data-testid=confirm]')
+      .should('be.visible')
+      .and('be.enabled')
+      .click()
+
+    cy.wait('@regenerate')
+
+    cy.get('[data-testid=app-passwords-reveal]').first().click()
+
+    cy.get('@password1').then((password1) => {
+      cy.get('[data-testid=app-passwords-value]')
+        .invoke('text')
+        .then((password) => {
+          expect(password).to.have.length(32)
+          expect(password).to.not.eq(password1)
+          expect(password).to.not.eq(HIDDEN_PASSWORD)
+        })
+    })
+
+    cy.log('It can generate a second password')
     cy.get('[data-testrole=regenerate]').eq(1).click()
     cy.wait('@regenerate')
 
-    cy.log('It only shows 1 password at a time')
+    cy.log('It only shows one password at a time')
+
     cy.get('[data-testrole=regenerate]').first().click()
+
+    cy.get('[data-testid=confirm]')
+      .should('be.visible')
+      .and('be.enabled')
+      .click()
+
     cy.wait('@regenerate')
-    cy.get('.pf-v5-c-alert__title')
-      .invoke('text')
-      .then((title) => {
-        title.includes('Email Password')
-      })
-    cy.get('[data-testid=app-passwords-value]').contains(HIDDEN_PASSWORD)
+
+    cy.get('.pf-v5-c-alert__title').should('contain.text', 'Email Password')
+
+    cy.get('[data-testid=app-passwords-value]').should(
+      'contain.text',
+      HIDDEN_PASSWORD,
+    )
+
     cy.get('[data-testrole=regenerate]').eq(1).click()
+
+    cy.get('[data-testid=confirm]')
+      .should('be.visible')
+      .and('be.enabled')
+      .click()
+
     cy.wait('@regenerate')
-    cy.get('.pf-v5-c-alert__title')
-      .invoke('text')
-      .then((title) => {
-        title.includes('smsPassword')
-      })
-    cy.get('[data-testid=app-passwords-value]').contains(HIDDEN_PASSWORD)
+
+    cy.get('.pf-v5-c-alert__title').should('contain.text', 'SmsPassword')
+
+    cy.get('[data-testid=app-passwords-value]').should(
+      'contain.text',
+      HIDDEN_PASSWORD,
+    )
   })
 })
